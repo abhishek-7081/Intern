3af6bc057235174b82b04b84753cf16965b6b288
abhishek-7081 <luckydwivedi7081@gmail.com>
2026-01-14 01:52:33 +0530

    depoyment settings

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://intern-75f8.onrender.com/api",
   withCredentials: true
 });
